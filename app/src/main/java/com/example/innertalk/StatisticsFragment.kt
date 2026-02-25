708c5176ab88618a4bada249ac3376aa2a28e661
Lucia17092006 <vegable21@gmail.com>
2026-02-25 09:20:03 +0100

    grafico conectado a la base a tiempo real

--- app/src/main/java/com/example/innertalk/StatisticsFragment.kt
+++ app/src/main/java/com/example/innertalk/StatisticsFragment.kt
@@ -5,19 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import android.widget.Toast.LENGTH_SHORT
 import com.example.innertalk.databinding.FragmentStatisticsBinding
 import com.github.aachartmodel.aainfographics.aachartcreator.AAChartModel
 import com.github.aachartmodel.aainfographics.aachartcreator.AAChartType
 import com.github.aachartmodel.aainfographics.aachartcreator.AASeriesElement
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 
 class StatisticsFragment : Fragment() {
 
     // 1. El _binding es el que realmente guarda la referencia y es nuleable
     private var _binding: FragmentStatisticsBinding? = null
 
-    // 2. Esta propiedad 'binding' solo es válida entre onCreateView y onDestroyView
-    // El get() !! asegura que no tengas que poner '?' en todo tu código
-    private val binding get() = _binding!!
+    private val binding get() = _binding!! // para no pones ? en todo el codigo
+
+    //Referencias  Firebase
+    private val auth = FirebaseAuth.getInstance()
+    private val database = FirebaseDatabase.getInstance("https://innertalk-ca928-default-rtdb.europe-west1.firebasedatabase.app/").reference
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -33,25 +43,66 @@ class StatisticsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         // 3. Aquí ya puedes usar 'binding' para configurar tu gráfico
-        configurarGrafico()
+        //configurarGrafico()
+        //Habra que borrar est llamada y sustituarla por leerEstadisticas
+        leerEstadisticasFirebase()
+
+
     }
 
-    private fun configurarGrafico() {
+    private fun  leerEstadisticasFirebase(){
+        val uid = auth.currentUser?.uid ?: return
+
+        //apuntamos al nodo de estadisticas dentro de Firebase
+        val statsRef = database.child("usuarios").child(uid).child("estadisticas")
+
+        //Vamos a  usar addValueEventListener para que el grafico pueda actualizarse en tiempo rea
+        statsRef.addValueEventListener(object  : ValueEventListener{
+            override fun onDataChange (snapshot: DataSnapshot){
+                // Extraemos los valores, si aún no existen (el usuario no ha hecho ninguna entrada) ponemos 0
+                if(snapshot.exists()){
+                    val veryHappy = snapshot.child("emocion_1").getValue(Int::class.java)?:0
+                    val happy = snapshot.child("emocion_2").getValue(Int::class.java) ?: 0
+                    val neutral = snapshot.child("emocion_3").getValue(Int::class.java) ?: 0
+                    val sad = snapshot.child("emocion_4").getValue(Int::class.java) ?: 0
+                    val angry = snapshot.child("emocion_5").getValue(Int::class.java) ?: 0
+
+                    //llamamos a la funcion para actualizar los datos del grafico
+                    actualizarGrafico(veryHappy, happy, neutral, sad, angry)
+                }else{
+                    Toast.makeText(requireContext(), "No hay datos en Firebase aún", Toast.LENGTH_SHORT).show()
+                    // Dibujamos uno vacío para que al menos se vea el título
+                    actualizarGrafico(0, 0, 0, 0, 0)
+                }
+
+            }
+
+            // si hay un error al obtener los datos de la base
+
+            override fun onCancelled(p0: DatabaseError) {
+                Toast.makeText(context, "Error al cargar los datos", LENGTH_SHORT).show()
+            }
+
+
+        })
+    }
+
+    private fun actualizarGrafico(v1: Int, v2: Int, v3: Int, v4: Int, v5: Int) {
         val aaChartModel: AAChartModel = AAChartModel()
             .chartType(AAChartType.Pie)
             .title("Análisis de Emociones")
             .backgroundColor("#F5F9FF")
             .dataLabelsEnabled(true)
-            //.colorsTheme() para añadir colores deseados
+            .colorsTheme(arrayOf("#4CAF50", "#FFEB3B", "#9E9E9E", "#FF9800", "#F44336")) //para añadir colores deseados
             .series(arrayOf(
                 AASeriesElement()
                     .name("Frecuencia")
                     .data(arrayOf(
-                        arrayOf("Felicidad", 10),
-                        arrayOf("Calma", 7),
-                        arrayOf("Apático", 3),
-                        arrayOf("Triste", 2),
-                        arrayOf("Enfado",4)
+                        arrayOf("Muy Feliz", v1),
+                        arrayOf("Contento", v2),
+                        arrayOf("Apático", v3),
+                        arrayOf("Triste", v4),
+                        arrayOf("Enfadado",v5)
                     ))
             ))
 
