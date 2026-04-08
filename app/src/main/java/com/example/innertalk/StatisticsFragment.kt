0d646bedf158bd1ad355390f05a5a7a1f5b5be72
Lucia17092006 <vegable21@gmail.com>
2026-04-08 09:27:30 +0200

    generación de mensajes segín emoción más frecuente

--- app/src/main/java/com/example/innertalk/StatisticsFragment.kt
+++ app/src/main/java/com/example/innertalk/StatisticsFragment.kt
@@ -72,11 +72,13 @@ class StatisticsFragment : Fragment() {
                     //llamamos a la funcion para actualizar los datos del grafico
                     actualizarGrafico(veryHappy, happy, neutral, sad, angry)
 
-                    //vamos a hacer un mapa con clave valo r
+                    generarAnalisisEmocional(veryHappy, happy, neutral, sad, angry)
                 }else{
                     Toast.makeText(requireContext(), "No hay datos en Firebase aún", Toast.LENGTH_SHORT).show()
                     // Dibujamos uno vacío para que al menos se vea el título
                     actualizarGrafico(0, 0, 0, 0, 0)
+
+                    generarAnalisisEmocional(0, 0, 0, 0, 0)
                 }
 
 
@@ -115,6 +117,74 @@ class StatisticsFragment : Fragment() {
         // Acceso directo gracias al binding
         binding.aaChartView.aa_drawChartWithChartModel(aaChartModel)
     }
+
+    private fun generarAnalisisEmocional (v1: Int, v2: Int, v3: Int, v4: Int, v5: Int){
+        //1. Guardamos los valores en un mapa para saber a que emoción correcponde cada número
+        val emociones = mapOf(
+            "Muy Feliz" to v1,
+            "Contento" to v2,
+            "Apático" to v3,
+            "Triste" to v4,
+            "Enfadado" to v5
+        )
+
+        //2.Buscamos que emoción tiene el valor máximo
+        val maxValor = emociones.values.maxOrNull() ?:0
+
+        //Si todas las emociones son 0 (el usuario no tiene registro)
+        //mostramos un mensaje de bienvenida para el usuario
+        if(maxValor==0){
+            binding.tvEmotionTitle.text="¡Empieza tu viaje!"
+            binding.tvAnalysisText.text="Registra como te sientes hoy en tu diario para comenzar tu análisi emocional"
+            return
+        }
+
+        //3. Obtenemos las emociones que tiene el valor máximo (puede haber empates)
+        val emocionesDominantes = emociones.filter { it.value == maxValor }.keys.toList() //el .keys extrae el nombre de las emociones
+
+        //4. Generamos el título y el análisi dependiendo de que emoción es más frecuente
+        //Si hay un empate cogeremos la primera de la lista
+        val emocionPrincipal = emocionesDominantes.first()
+        val titulo : String
+        val analisis : String
+
+        when (emocionPrincipal) {
+            "Muy Feliz", "Contento" -> {
+                titulo = "¡Estás en una buena racha!  ☀\uFE0F"
+                analisis =
+                    "Tu emoción predominante es '$emocionPrincipal'. Parece que últimamente estás encontrando motivos para sonreír. ¡Sigue así! Recuerda guardar estos buenos momentos en tu Diario de Gratitud."
+            }
+
+            "Apático" -> {
+                titulo = "Días de calma plana ☁☁\uFE0F"
+                analisis =
+                    "Tu emoción más frecuente es '$emocionPrincipal'. Es normal tener temporadas donde todo da un poco igual. Intenta probar alguna actividad nueva, como 'Caminar sin Rumbo', para despertar un poco tus sentidos."
+            }
+
+            "Triste" -> {
+                titulo = "Abrazo virtual en camino \uD83D\uDC9A"
+                analisis =
+                    "He notado que la tristeza ha estado muy presente. No pasa nada por sentirse así. Apóyate en el chat de InnerTalk si necesitas desahogarte, o prueba una meditación guiada."
+            }
+
+            "Enfadado" -> {
+                titulo = "Tensión acumulada 🌩\uD83C\uDF29\uFE0F"
+                analisis =
+                    "El enfado es tu emoción principal ahora mismo. Quizás estás pasando por mucho estrés. Intenta hacer la actividad 'Estiramiento Exprés' o la 'Respiración 4-7-8' para soltar esa carga."
+            }
+
+            else -> {
+                titulo = "Analizando tus emociones..."
+                analisis = "Sigue registrando tu estado de ánimo para obtener un informe detallado."
+            }
+
+        }
+        //5. Actualizamos los textos en la pantalla
+        binding.tvEmotionTitle.text = titulo
+        binding.tvAnalysisText.text = analisis
+    }
+
+
 
     override fun onDestroyView() {
         super.onDestroyView()
